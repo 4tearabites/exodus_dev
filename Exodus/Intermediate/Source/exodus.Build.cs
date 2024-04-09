3c403991ea12c5ea4a20039f560db29abdf2a498
4tearabites <cbruns9994@tamu.edu>
2024-04-09 15:10:35 +0200

    g

---
+++ Exodus/Intermediate/Source/exodus.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class exodus : ModuleRules
+{
+	public exodus(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
